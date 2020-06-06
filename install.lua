3f288e3f37074de0e9881c80e34b6c6bffc54990
DCBURG3R <53492550+DCBURG3R@users.noreply.github.com>
2020-06-06 13:33:45 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,8 +2,8 @@
 --
 --
 
-local libURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/lib/f.lua"
-local startupURL = "https://raw.githubusercontent.com/acidjazz/drmon/master/drmon.lua"
+local libURL = "https://raw.githubusercontent.com/DCBURG3R/drmon/master/lib/f.lua"
+local startupURL = "https://raw.githubusercontent.com/DCBURG3R/drmon/master/drmon.lua"
 local lib, startup
 local libFile, startupFile
 
